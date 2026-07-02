873aee4f127e2b9bd0bbcf193b2c8535c6141f20
tchongwang bakayoko <tchongwangbakayoko619@gmail.com>
2026-07-02 11:26:36 +0100

    front end tasks

--- task_api/task_api/settings.py
+++ task_api/task_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h=azl5m45y506-$8lou_)9yi^js8cvve0q_a6x4*6opxtcmt1s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
